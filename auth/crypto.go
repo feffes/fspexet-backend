07e9769671404dd623fbf863f2e834290701422b
feffe <feffe@f-spexet.se>
2019-01-28 21:22:30 +0100

    Partial argon implementation

--- auth/crypto.go
+++ auth/crypto.go
@@ -1,13 +1,25 @@
 package auth
 
 import (
+	"errors"
+	"regexp"
 	"os"
 	"crypto/rand"
 	"crypto/rsa"
 )
+const argonRegex = `^(?:\$([a-z0-9-]+))(?:\$v=([a-z0-9]+))?(?:\$((?:[a-z0-9-]+=[a-zA-Z0-9/+.-]+,?)+))?(?:\$([a-zA-Z0-9/+.-]+))?(?:\$([a-zA-Z0-9/+.-]+))?$`
 
 var keyDir os.File
 var oldKeyDir os.File
+
+type argon struct{
+	id 		string
+	ver 	string
+	conf	string
+	salt 	string
+	hash	string
+}
+
 //GenRS4096KeyPair returns privatekey, publickey
 func GenRS4096KeyPair() error{
 	key, err := rsa.GenerateKey(rand.Reader, 4096)
@@ -19,4 +31,21 @@ func GenRS4096KeyPair() error{
 	PublicKey = &key.PublicKey
 
 	return nil
+}
+// CompareArgon2id compares an input string to a given argon2id hash
+
+
+func parseArgon(hash string) (argon, error){
+	rex := regexp.MustCompile(argonRegex)
+	out := rex.FindAllStringSubmatch(hash, -1)[0]
+	if hash != out[0] || len(out) != 6{
+		return argon{}, errors.New("invalid argon hash:" + hash)
+	}
+	return argon{
+		id: out[1],
+		ver: out[2],
+		conf: out[3],
+		salt: out[4],
+		hash: out[5],
+	}, nil
 }
